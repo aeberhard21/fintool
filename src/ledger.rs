bf0fae0e1b90038c1efe02005ebc99b387967910
aeberhard21 <aje3956@g.rit.edu>
2023-11-12 14:39:26 -0500

    able to accept user input to create ledger

--- src/ledger.rs
+++ src/ledger.rs
@@ -4,6 +4,7 @@ pub struct LedgerEntry {
     pub date: String, 
     pub amount: f32, 
     pub deposit: bool,
+    pub payee: String,
     pub description: String,
 }
 
@@ -27,6 +28,21 @@ impl Ledger {
     pub fn sum(&self) -> f32 {
         self.sum
     }
+
+    pub fn print(&self) {
+        let width = 16;
+        let precision = 2;
+        println!("{:width$}| +/-| {:width$}| {:width$}| {:width$}", "Date", "Payee", "Amount", "Description");
+        println!("-------------------------------------------------");
+        for entry in &self.entries {
+            if entry.deposit { 
+                println!("{:width$}|   +| {:width$}| {:width$.precision$}| {:width$}",  entry.date, entry.payee, entry.amount, entry.description);
+            } else {
+                println!("{:width$}|   -| {:width$}| {:width$.precision$}| {:width$}",  entry.date, entry.payee, entry.amount, entry.description);
+            }
+        }
+    }
+
     // private methods
     fn update_sum(&mut self) {
         let mut sum : f32 = 0.0;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,75 @@
+use core::panic;
+
 use crate::ledger::Ledger;
 use crate::ledger::LedgerEntry;
+use chrono::{NaiveDate, Weekday};
+use inquire::*;
 
 mod ledger;
 
 fn main() {
-    println!("Hello, world!");
+
     let mut _ledger: Ledger = Ledger::new();
-    let entry = LedgerEntry {
-        date: String::from("November 9, 2022"), 
-        amount: 32000.42,
-        deposit: false, 
-        description: String::from("car payment")
-    };
-    _ledger.add(entry);
+    let deposit_options: Vec<&str>= vec!["Credit", "Debit"];
+
+    loop {
+        // this function returns either "Ok" or "Err". "Ok" indicates that the type T in Result<T, E>
+        // is okay to be used. 
+        let date_input: Result<NaiveDate, InquireError> = DateSelect::new("Enter date").prompt();
+        let date: String = date_input.unwrap().to_string();
+
+        println!("Entered date is {0}", date);
+
+        let amount_input: Result<f32, InquireError> = CustomType::<f32>::new("Enter amount")
+            .with_placeholder("00000.00")
+            .with_default(00000.00)
+            .with_error_message("Please type a valid amount!")
+            .prompt();
+        let amount: f32 = amount_input.unwrap();
+
+        println!("Entered amount is {}", amount.to_string());
+
+        let deposit_type : String = Select::new("Credit or debit:", deposit_options).prompt().unwrap().to_string();
+        let mut deposit : bool = false;
+
+        let mut payee : String = "".to_string();
+
+        // the match is equivalent to a switch statement
+        match deposit_type.as_str() {
+            "Credit" => {
+                deposit = false;
+                payee = Text::new("Enter payee:").prompt().unwrap().to_string();
+            }
+            "Debit" => {
+                deposit = true;
+            } 
+            _ => {
+                panic!("Invalid entry.");
+            }
+        }
+
+        let description_input : String = Text::new("Enter payment description:").prompt().unwrap().to_string();
+
+        let entry = LedgerEntry {
+            date: date, 
+            amount: amount,
+            deposit: deposit,
+            payee: payee,
+            description: description_input
+        };
+        _ledger.add(entry);
+
+        let another : bool = Confirm::new("Add another entry?")
+            .with_default(false)
+            .prompt()
+            .unwrap();
+        if !another {
+            break;
+        } else {
+            println!("");
+        }
+    }
+    println!("");
     println!("My new ledger sum is {0}", _ledger.sum());
+    _ledger.print();
 }
